8db898a41dec35a51c10b24d85137c0e07759b65
Sombit Pramanik <m.sombitpramanik@gmail.com>
2024-07-28 09:04:15 +0530

    activate the Members page Nav Link

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ def Index():
     return render_template("index.html")
 
 
+@app.route("/Members")
+def Members():
+    return render_template("members.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
